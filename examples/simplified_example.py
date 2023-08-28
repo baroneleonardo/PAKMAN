9a61c5b12915c3713fc075384fc740baf652bc24
Kanthavel Pasupathipillai <vysybyl@gmail.com>
2023-08-28 10:05:02 +0200

    Run additional tests

--- examples/simplified_example.py
+++ examples/simplified_example.py
@@ -43,38 +43,38 @@ N_RANDOM_WALKERS = 12 * 2  # Originally fixed at 2 ** 4 = 16
 #                                          np.arange(0, 1, 0.005),
 #                                          np.arange(0, 1, 0.005))
 
-objective_func_name = 'Query26'
-objective_func = precomputed_functions.Query26
-known_minimum = objective_func.minimum
-domain = objective_func
-# SUGGESTED:
-N_INITIAL_POINTS = 7
-N_ITERATIONS = 15
-N_POINTS_PER_ITERATION = 4  # The q- parameter
-M_DOMAIN_DISCRETIZATION_SAMPLE_SIZE = 8  # M parameter
-N_RANDOM_WALKERS = 2 ** 4
+# objective_func_name = 'Query26'
+# objective_func = precomputed_functions.Query26
+# known_minimum = objective_func.minimum
+# domain = objective_func
+# # SUGGESTED:
+# N_INITIAL_POINTS = 7
+# N_ITERATIONS = 15
+# N_POINTS_PER_ITERATION = 4  # The q- parameter
+# M_DOMAIN_DISCRETIZATION_SAMPLE_SIZE = 8  # M parameter
+# N_RANDOM_WALKERS = 2 ** 4
 
 # objective_func_name = 'LiGen'
 # objective_func = precomputed_functions.LiGen
 # known_minimum = objective_func.minimum
 # domain = objective_func
 # # SUGGESTED:
-# N_INITIAL_POINTS = 5
-# N_ITERATIONS = 25
-# N_POINTS_PER_ITERATION = 5  # The q- parameter
-# M_DOMAIN_DISCRETIZATION_SAMPLE_SIZE = 12  # M parameter
+# N_INITIAL_POINTS = 8
+# N_ITERATIONS = 15
+# N_POINTS_PER_ITERATION = 8  # The q- parameter
+# M_DOMAIN_DISCRETIZATION_SAMPLE_SIZE = 16  # M parameter
 # N_RANDOM_WALKERS = 2 ** 4  # Originally fixed at 2 ** 4 = 16
 
-# objective_func_name = 'StereoMatch'
-# objective_func = precomputed_functions.StereoMatch
-# known_minimum = objective_func.minimum
-# domain = objective_func
-# # # SUGGESTED:
-# N_INITIAL_POINTS = 4
-# N_ITERATIONS = 15
-# N_POINTS_PER_ITERATION = 4  # The q- parameter
-# M_DOMAIN_DISCRETIZATION_SAMPLE_SIZE = 8  # M parameter
-# N_RANDOM_WALKERS = 2 ** 4
+objective_func_name = 'StereoMatch'
+objective_func = precomputed_functions.StereoMatch
+known_minimum = objective_func.minimum
+domain = objective_func
+# # SUGGESTED:
+N_INITIAL_POINTS = 4
+N_ITERATIONS = 15
+N_POINTS_PER_ITERATION = 4  # The q- parameter
+M_DOMAIN_DISCRETIZATION_SAMPLE_SIZE = 12  # M parameter
+N_RANDOM_WALKERS = 2 ** 4
 
 ###############################
 # Initializing utility objects
